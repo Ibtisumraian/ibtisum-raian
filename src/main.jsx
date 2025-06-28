1194ec5d768fafbc556b27182ee25ac8244b7006
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-28 19:22:16 +0600

    sated up routes and hosted in firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { router } from '../components/Routes/Routes.jsx'
 import { RouterProvider } from 'react-router'
+import { router } from './components/Routes/Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
