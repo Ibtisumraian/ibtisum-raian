8713cb1612fd15df5b9377c0c1772b4c490012bf
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-30 03:25:54 +0600

    added responsive hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,17 +5,17 @@ import { FaXTwitter } from 'react-icons/fa6';
 const Hero = () => {
     return (
         <div className='w-10/12 mx-auto py-12 '>
-            <div className='flex items-center justify-between gap-3 '>
-                <div className='text-white flex flex-col gap-3'>
+            <div className='flex items-center flex-col-reverse lg:flex-row lg:justify-between gap-3 '>
+                <div className='text-white flex flex-col gap-3 text-center lg:text-start'>
                     <h1 className='text-5xl 2xl:text-7xl font-bold'>Ibtisum Raian</h1>
                     <h1 className='text-5xl 2xl:text-7xl font-bold'>Full Stack Developer</h1>
                     <p className='max-w-[700px] my-8 text-lg'>I'm a passionate MERN Stack Developer focused on building high-performance, full-stack web applications. With a strong eye for detail and a love for clean code, I turn complex ideas into seamless digital experiences — from intuitive frontends to robust backends.</p>
-                    <div className='flex items-center gap-5'>
+                    <div className='flex items-center justify-center lg:justify-start gap-5'>
                         <a href='https://github.com/Ibtisumraian' target='blank' className='bg-gradient-to-b from-[#e9615e]  to-[#ec9956] p-2 rounded-lg text-lg  '><FaGithub /></a>
                         <a href='https://x.com/ib_raian' target='blank' className='bg-gradient-to-b from-[#e9615e]  to-[#ec9956] p-2 rounded-lg text-lg  '><FaXTwitter /></a>
                         <a href='https://www.linkedin.com/' target='blank' className='bg-gradient-to-b from-[#e9615e]  to-[#ec9956] p-2 rounded-lg text-lg  '><FaLinkedin /></a>
                     </div>
-                    <a className="w-fit text-2xl py-2 px-6 mt-8 rounded-4xl drop-shadow-xl/20  bg-gradient-to-b from-[#e9615e]  to-[#ec9956] flex items-center gap-3 cursor-pointer"> <FaDownload className='text-lg' />Resume</a>
+                    <a className="w-fit mx-auto lg:mx-0 text-2xl py-2 px-6 mt-8 rounded-4xl drop-shadow-xl/20  bg-gradient-to-b from-[#e9615e]  to-[#ec9956] flex items-center gap-3 cursor-pointer"> <FaDownload className='text-lg' />Resume</a>
                     
                 </div>
                 <div className='flex items-center gap-3'>
@@ -23,7 +23,7 @@ const Hero = () => {
                         className='rounded-t-full rounded-b-full max-h-[750px]'
                         src="https://res.cloudinary.com/dv6p7mprd/image/upload/v1751123062/dp001_wntiwg.png" alt="" />
                     <img
-                        className='h-[300px]'
+                        className='h-[300px] hidden lg:block'
                         src="https://res.cloudinary.com/dv6p7mprd/image/upload/v1751126278/my-name-removebg-preview_k5rnpe.png" alt="" />
                     {/* <img
                         className='rounded-t-full rounded-b-full max-h-[750px]'
